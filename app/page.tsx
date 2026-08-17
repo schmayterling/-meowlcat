4b68d8a0ea485d799e4389217e0fd610d3fefb71
aluminyoom <ryana@ryanaque.com>
2026-08-17 10:27:48 +0800

    meowl

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="viewport-art">
+      <img
+        src="/cat.png"
+        alt="a wide-eyed cat perched against a green background"
+      />
+    </main>
+  );
+}
